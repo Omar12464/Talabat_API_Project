3fbb7dbd75b69207ef19dab77dae0ac954a3292e
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-10-23 16:07:22 +0300

    OrderEntites

---
+++ Talabat_Core/Order_Aggregate/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat_Core.Models;
+
+namespace Talabat_Core.Order_Aggregate
+{
+    public class Order:ModelBase
+    {
+        public Order()
+        {
+            
+        }
+        public Order(string buyerEmail, OrderStatus status, Address shippingAddress, DeliveryMethod _deliveryMethod, ICollection<OrderItem> items, decimal subTotal, decimal total)
+        {
+            BuyerEmail = buyerEmail;
+            Status = status;
+            ShippingAddress = shippingAddress;
+            deliveryMethod = _deliveryMethod;
+            Items = items;
+            SubTotal = subTotal;
+            Total = total;
+        }
+
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatus Status { get; set; }
+        public Address ShippingAddress { get; set; }
+        //public int DeliveryMethod { get; set; }
+        public DeliveryMethod deliveryMethod { get; set; }
+
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+        public decimal SubTotal { get; set; }//OrderPrice without Delivery Method
+        public decimal Total { get; }//SubTotal+Delivery Method
+
+        public decimal GetTotal()
+        {
+            return SubTotal + Total;
+        }
+        public string PaymentIntentId { get; set; }
+    }
+}
---
+++ Talabat_Core/Order_Aggregate/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat_Core.Models;
+
+namespace Talabat_Core.Order_Aggregate
+{
+    public  class OrderItem:ModelBase
+    {
+        public OrderItem()
+        {
+            
+        }
+        public OrderItem(ProductItemOrder productItem, decimal price, int quantity)
+        {
+            this.productItem = productItem;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public ProductItemOrder productItem { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
