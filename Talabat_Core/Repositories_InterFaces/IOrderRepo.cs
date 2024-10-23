6dade7699ecdb0e82983c166a81cac74e88d943a
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-10-23 17:38:15 +0300

    Setup IorderService

---
+++ Talabat_Core/Repositories_InterFaces/IOrderRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat_Core.Order_Aggregate;
+
+namespace Talabat_Core.Repositories_InterFaces
+{
+    public interface IOrderRepo
+    {
+        Task<Order> CreateOrderAsync(string buyerEmail, string basketId, int deliveryMethod, Address shippingAddress);
+        Task<IReadOnlyList<Order>> GetOrderForUserAsync(string buyerEmail);
+        Task<Order> CreateOrderByIdForUserAsync(int orderId, string buyerEmail);
+
+
+    }
+}
---
+++ Talabat_Service/OrderService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat_Core.Order_Aggregate;
+using Talabat_Core.Repositories_InterFaces;
+
+namespace Talabat_Service
+{
+    public class OrderService : IOrderRepo
+    {
+        public Task<Order> CreateOrderAsync(string buyerEmail, string basketId, int deliveryMethod, Address shippingAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> CreateOrderByIdForUserAsync(int orderId, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrderForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
